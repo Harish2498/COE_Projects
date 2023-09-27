ecc29e151e49321f25a94896b5c88dc1e5696df4
Harish Sharma <harish.s@idsil.com>
2023-09-27 18:42:53 +0530

    logo

--- src/Components/data.js
+++ src/Components/data.js
@@ -4,16 +4,16 @@ const projects = [
         Link: "http://13.126.173.226/",
         Description: "The Todo Task Management System is a comprehensive web-based application designed to streamline task management processes within an organization. This project aims to provide a centralized platform where employees can view their assigned tasks, managers can assign tasks to employees, and administrators have full control over user and task management."
     },
-    {
-        Name: "HDA-(Health Diagnosis Application)",
-        Link: "",
-        Description: "In this application we are detection covid by the help of lungs image and symptoms which are filled by user. This application is in React,Python and ML."
-    },
-    {
-        Name: "PDS -(Publishing Digital Suits)",
-        Link: "",
-        Description: "Plan, write, and edit your next masterpiece from start to finish in a fully-featured platform packed with in-depth, interactive editing tools that build your confidence and let your stories shine."
-    },
+    // {
+    //     Name: "HDA-(Health Diagnosis Application)",
+    //     Link: "",
+    //     Description: "In this application we are detection covid by the help of lungs image and symptoms which are filled by user. This application is in React,Python and ML."
+    // },
+    // {
+    //     Name: "PDS -(Publishing Digital Suits)",
+    //     Link: "",
+    //     Description: "Plan, write, and edit your next masterpiece from start to finish in a fully-featured platform packed with in-depth, interactive editing tools that build your confidence and let your stories shine."
+    // },
     {
         Name: "Twitter - Airline data sentiments analysis",
         Link: "https://sentiment-analysis-app-97yn60a23uv.streamlit.app/",
@@ -33,6 +33,11 @@ const projects = [
         Name: "WhatsApp - Group chat sentiment analysis",
         Link: "https://wbs9300-hsvhwtgep8ctxmyydjxp3y.streamlit.app/",
         Description: "Sentiment Analysis of Whatsapp Chat for example,Postive,Negative,Neutral emoji's used,words used,user to user comparision etc."
+    },
+    {
+        Name:"JMS - Job Management system",
+        Link:"http://52.45.162.56/",
+        Description:"Job tracking and management processes."
     }
 
 ]
